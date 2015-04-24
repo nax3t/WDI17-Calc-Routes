a067e27078af83b6b57abec88ade33e14eb67daa
Ian <iantskon@gmail.com>
2015-04-23 19:38:41 -0700

    Add remaining methods

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,4 +8,26 @@ class WelcomeController < ApplicationController
   	@result = x.to_i + y.to_i
   	render :index
   end
+
+  def sub
+  	x = params[:x]
+  	y = params[:y]
+  	@result = x.to_i - y.to_i
+  	render :index
+  end
+
+  def div
+  	x = params[:x]
+  	y = params[:y]
+  	@result = x.to_i / y.to_i
+  	render :index
+  end
+
+  def mult
+  	x = params[:x]
+  	y = params[:y]
+  	@result = x.to_i * y.to_i
+  	render :index
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'welcome/index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -10,6 +7,9 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   get 'add/:x/:y' => 'welcome#add'
+  get 'sub/:x/:y' => 'welcome#sub'
+  get 'div/:x/:y' => 'welcome#div'
+  get 'mult/:x/:y' => 'welcome#mult'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
